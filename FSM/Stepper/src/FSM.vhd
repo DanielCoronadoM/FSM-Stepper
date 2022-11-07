4fad04d4cadfb0528a6d769697dd7857e6ccf59b
Daniel_C <danhc7417@gmail.com>
2022-11-07 00:02:02 -0600

    FSM prueba 1

--- FSM/Stepper/src/FSM.vhd
+++ FSM/Stepper/src/FSM.vhd
@@ -1,5 +1,8 @@
 --Algorith state machine
 
+-- dir = 1 : Giro en sentido horario
+-- dir = 0 : Giro en sentido antihorario
+
 library ieee;
 use ieee.std_logic_1164.all; --> Operacione logicas
 
@@ -15,7 +18,7 @@ end entity;
 
 architecture functional of FSM is
 
-signal Qp, Qf	:std_logic_vector(3 downto 0):="0000";	 --hexadecimal
+signal Qp, Qf	:std_logic_vector(3 downto 0):="0000";	 
 
 begin
 	
@@ -29,7 +32,9 @@ begin
 	begin
 		if rising_edge(clk) then  --Detector de flancos positivos
 			if (rst = '1') then
-				Qp <=  x"1"; --S0
+				Qp <=  x"1"; --Step_1
+				
+				--Valores de reseteo
 				a	<= '0';
 				b	<= '0';
 				c	<= '0';
@@ -38,44 +43,123 @@ begin
 			elsif (ena = '1') then
 				Qp <= Qf;
 				
+				--Valores predeterminados
+				a	<= '0';
+				b	<= '0';
+				c	<= '0';
+				d	<= '0';
 				
-			Case Qp is		 --Se usa formato hexadecimal
+			Case Qp is		 --
 			
-			------------------------------S0
-			when x"1" => 	--S0
+			------------------------------Step_1
+			when x"1" => 	
+				a	<= '1';
+				b	<= '0';
+				c	<= '0';
+				d	<= '0';
+				
 				if(dir = '1')then
 					Qf <= x"2";	--S2
 				else
-					Qf <= x"8";	--S3
+					Qf <= x"8";	--S8
 				end if;
 				
-			-----------------------------S1
+			-----------------------------Step_2
 			when x"2" =>
+				a	<= '1';
+				b	<= '1';
+				c	<= '0';
+				d	<= '0';
+				
 				if(dir = '1')then
-					Qf <= x"4"; --S2
+					Qf <= x"3"; --S3
 				else
-					Qf <= x"1"; --S0
+					Qf <= x"1"; --S1
 				end if;
 		
-			-----------------------------S2
-			when x"4" =>
+			-----------------------------Step_3
+			when x"3" =>
+				a	<= '0';
+				b	<= '1';
+				c	<= '0';
+				d	<= '0';
+				
 				if(dir = '1')then
-					Qf <= x"8"; --S3
+					a	<= '0';
+					b	<= '1';
+					c	<= '0';
+					d	<= '0';
+					Qf <= x"4"; --S4
 				else
-					Qf <= x"2"; --S1
+					Qf <= x"2"; --S2
+				end if;
+				
+			-----------------------------Step_4-------------------
+			when x"4" =>
+				a	<= '0';
+				b	<= '1';
+				c	<= '1';
+				d	<= '0';
+				
+				if(dir = '1')then
+					Qf <= x"5"; --S5
+				else
+					Qf <= x"3"; --S3
+				end if;
+				
+			-----------------------------Step_5
+			when x"5" =>
+				a	<= '0';
+				b	<= '0';
+				c	<= '1';
+				d	<= '0';
+				
+				if(dir = '1')then
+					Qf <= x"6"; --S6
+				else
+					Qf <= x"4"; --S4
+				end if;
+				
+			-----------------------------Step_6
+			when x"6" =>
+				a	<= '0';
+				b	<= '0';
+				c	<= '1';
+				d	<= '1';
+				
+				if(dir = '1')then
+					Qf <= x"7"; --S7
+				else
+					Qf <= x"5"; --S5
+				end if;
+				
+			-----------------------------Step_7
+			when x"7" =>
+				a	<= '0';
+				b	<= '0';
+				c	<= '0';
+				d	<= '1';
+				
+				if(dir = '1')then
+					Qf <= x"8"; --S8
+				else
+					Qf <= x"6"; --S6
 				end if;
 			
-			------------------------------S3
+			------------------------------Step_8
 			when others =>
+				a	<= '1';
+				b	<= '0';
+				c	<= '0';
+				d	<= '1';
 				if(dir = '1')then
-					Qf <= x"1"; --S0
+					Qf <= x"1"; --S1
 				else
-					Qf <= x"4"; --S2
+					Qf <= x"7"; --S7
 				end if;
 				
 			end case;
 			end if;
 		end if;
-	end process;
-	
+	end process;	
 end architecture;
